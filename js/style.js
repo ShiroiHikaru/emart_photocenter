4b54ea3a4b362dbc871476753ae402fae3d95335
ShiroiHikaru <asd95121@naver.com>
2022-04-05 02:37:35 +0900

    css and javascript is so difficult

--- js/style.js
+++ js/style.js
@@ -7,12 +7,19 @@ document.cookie = 'cookie1=value1; SameSite=Lax';
 // Set a cross-site cookie for third-party contexts
 document.cookie = 'cookie2=value2; SameSite=None; Secure';
 
-// header 메뉴 drop
-function drop(){
-    const click = document.getElementById('menutab');
-    if(click.style.display === 'none'){
-       click.style.display = 'block';
-    }else{
-       click.style.display = 'none';
-    }
- }
+
+// header - 헤더 dropdown
+function show_list(){
+   let dropbox = document.getElementById("menu_list");
+
+   if(dropbox.style.display == "block"){
+      dropbox.style.display = "none";
+   } else {
+      dropbox.style.display = "block";
+   }
+}
+window.onclick = function(event){
+   if(!event.target.matches(".hammenu")){
+      document.getElementById("menu_list").style.display = "none"
+   }
+}
