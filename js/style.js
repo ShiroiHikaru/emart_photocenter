4a108b6199fc749c1561c429914022d9ca37c7d9
ShiroiHikaru <asd95121@naver.com>
2022-04-04 22:59:41 +0900

    header css and js

--- js/style.js
+++ js/style.js
@@ -6,3 +6,13 @@
 document.cookie = 'cookie1=value1; SameSite=Lax';
 // Set a cross-site cookie for third-party contexts
 document.cookie = 'cookie2=value2; SameSite=None; Secure';
+
+// header 메뉴 drop
+function drop(){
+    const click = document.getElementById('menutab');
+    if(click.style.display === 'block'){
+       click.style.display = 'none';
+    }else{
+       click.style.display = 'block';
+    }
+ }
