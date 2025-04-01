37295eb008aefe0e1f76b1e03c28d6eab2668cc1
ShiroiHikaru <asd9521@naver.com>
2025-04-01 21:59:09 +0900

    feat(emart): eslint 셋업

---
+++ eslint.config.js
@@ -0,0 +1,45 @@
+import globals from 'globals';
+import { defineConfig } from 'eslint/config';
+import js from '@eslint/js';
+
+export default defineConfig([
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    files: ['**/*.js'],
+    plugins: {
+      js,
+    },
+    extends: ['js/recommended'],
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-undef': 'warn',
+      'arrow-body-style': 'off',
+      'no-plusplus': 'off',
+      'no-restricted-exports': ['error', { restrictDefaultExports: { defaultFrom: false } }],
+      'no-param-reassign': ['error', { props: false }],
+      'no-unused-expressions': ['warn'],
+      'no-shadow': 'off',
+      'prefer-destructuring': ['error', { object: true, array: false }],
+      'lines-between-class-members': 'off',
+    },
+  },
+]);
+
+//   "rules": {
+//     "jsx-a11y/click-events-have-key-events": "off",
+//     "jsx-a11y/label-has-associated-control": [
+//       "error",
+//       {
+//         "labelComponents": ["label"],
+//         "labelAttributes": ["htmlFor"],
+//         "controlComponents": ["input"]
+//       }
+//     ]
+//   },
+//   "globals": { "navigation": "readonly", "globalThis": false, "NodeJS": true }
+// }
